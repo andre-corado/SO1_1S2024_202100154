4be53c493d1e6fe14af09e9d61a94dd74c1cb249
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-02-01 17:00:31 -0600

    Front

--- Tareas/Tarea1/react-app/src/App.tsx
+++ Tareas/Tarea1/react-app/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [data, setData] = useState<string>('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('URL_DE_TU_API'); // Reemplaza 'URL_DE_TU_API' con la URL real de tu API
+      const jsonData = await response.json();
+      setData(JSON.stringify(jsonData));
+    } catch (error) {
+      console.error('Error al obtener datos:', error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <header>
+        <h1>TAREA 1 - SO1 - 1S2024</h1>
+      </header>
+      <main>
+        <button onClick={fetchData}>Mostrar Datos</button>
+        {data && <p>{data}</p>}
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
