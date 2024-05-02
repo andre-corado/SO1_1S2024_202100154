a16d34dfa55d0e4f1506b3b40ef4f4e5bebb75a4
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-05-02 03:15:28 -0600

    go-app to Kafka

--- proyecto2/producers/grpc/servidor/go.mod
+++ proyecto2/producers/grpc/servidor/go.mod
@@ -4,12 +4,15 @@ go 1.22.2
 
 require (
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/segmentio/kafka-go v0.4.47
 	google.golang.org/grpc v1.63.2
 	google.golang.org/protobuf v1.33.0
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- proyecto2/producers/grpc/servidor/servidor.go
+++ proyecto2/producers/grpc/servidor/servidor.go
@@ -8,8 +8,12 @@ import (
 	pb "serverGRPC/proto"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/segmentio/kafka-go"
 	"google.golang.org/grpc"
 )
+
+var brokers = []string{"cluster-p2-kafka-bootstrap:9092"}
+var topic = "my-topic"
 
 type server struct {
 	pb.UnimplementedGetInfoServer
@@ -42,10 +46,30 @@ func (s *server) ReturnInfo(ctx context.Context, in *pb.RequestId) (*pb.ReplyInf
 
 func sendToKafka(album Data) {
 	// Envía los datos a Kafka
-	return
+	producer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers:  brokers,
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	})
+	defer producer.Close()
+
+	string := fmt.Sprintf("%s,%s,%s,%s", album.name, album.album, album.year, album.rank)
+	// to bytes
+	message := []byte(string)
+
+	// Create a Kafka message
+	kafkaMessage := kafka.Message{
+		Key:   nil,     // You can specify a key if you want
+		Value: message, // Your message content
+	}
+
+	// Write the message to Kafka
+	producer.WriteMessages(context.Background(), kafkaMessage)
+	fmt.Println("Mensaje enviado a Kafka")
 }
 
 func main() {
+
 	listen, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalln(err)
@@ -56,4 +80,17 @@ func main() {
 	if err := s.Serve(listen); err != nil {
 		log.Fatalln(err)
 	}
+
+	brokers := []string{"cluster-p2-kafka-bootstrap:9092"}
+	topic := "my-topic"
+
+	// Create a new producer
+	producer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: brokers,
+		Topic:   topic,
+	})
+	if producer != nil {
+		log.Fatalf("Error creating producer: %s", err)
+	}
+	defer producer.Close()
 }
