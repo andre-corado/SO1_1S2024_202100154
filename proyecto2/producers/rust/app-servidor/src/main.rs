d1b569bb6498e9e077332dcd79ac1df7738b0013
andre-corado <98116674+andre-corado@users.noreply.github.com>
2024-05-02 17:28:26 -0600

    DockerFile
    Rust_producer

--- proyecto2/producers/rust/app-servidor/src/main.rs
+++ proyecto2/producers/rust/app-servidor/src/main.rs
@@ -3,12 +3,11 @@ use rocket::serde::json::{json, Value as JsonValue};
 use rocket::serde::json::Json;
 use rocket::config::SecretKey;
 use rocket_cors::{AllowedOrigins, CorsOptions};
-use rdkafka::producer::{Producer, FutureProducer};
-use rdkafka::util::get_rdkafka_version;
-use rdkafka::config::ClientConfig;
+use rdkafka::producer::{FutureRecord, FutureProducer};
+use rdkafka::util::Timeout;
 
 
-#[derive(rocket::serde::Deserialize)]
+#[derive(rocket::serde::Deserialize, Clone)]
 struct Data {
     name: String,
     album: String,
@@ -18,31 +17,46 @@ struct Data {
 
 #[rocket::post("/data", data = "<data>")]
 fn receive_data(data: Json<Data>) -> Result<String, BadRequest<String>> {
-    let received_data = data.into_inner();
-    let response = JsonValue::from(json!({
+    let received_data = data.clone().into_inner();
+    let message = json!({
         "message": format!("Received data: Nombre: {}, Album: {}, Año: {}, Rank: {}", received_data.name, received_data.album, received_data.year, received_data.rank)
-    }));
+    });
     // Enviar mensaje a Kafka
-    send_kafka().await;
-    Ok(response.to_string())
+    send_kafka(data);
+    Ok(message.to_string())
 }
+
 
 
 // KAFKA -----------------------------------------------------------------------
 // Función para enviar mensajes a un tópico de Kafka
-async fn send_kafka() {
+async fn send_kafka(data: Json<Data>) {
     // Configura la configuración del cliente Kafka
-    let mut producer = ClientConfig::new()
+    let producer: FutureProducer = rdkafka::config::ClientConfig::new()
         .set("bootstrap.servers", "cluster-p2-kafka-bootstrap:9092")
         .set("message.timeout.ms", "2000")
-        .create::<FutureProducer>()
-        .expect("Producer creation error");
+        .create()
+    .expect("Producer creation error");
 
-    // Envia un mensaje al topic 'my-topic'
-    producer.send_copy::<str, str>("my-topic", Some("key"), Some("value"), None, None)
-        .await
-        .expect("Failed to produce message")
-        .unwrap();
+    // Formato del mensaje a enviar
+    let payload: String = format!("{},{},{},{}", data.name, data.album, data.year, data.rank);
+    let topic = "my-topic";
+    let delivery_status = producer.send(
+            FutureRecord::<String, _>::to(&topic)
+                .payload(&payload.as_bytes().to_vec()),
+            Timeout::Never,
+        ).await;
+
+
+    // Verificar el estado de entrega del mensaje
+    match delivery_status {
+        Ok(delivery_result) => {
+            println!("Mensaje enviado con éxito: {:?}", delivery_result);
+        }
+        Err((err, _msg)) => {
+            eprintln!("Error al enviar el mensaje: {:?}", err);
+        }
+    }
 }
 
 
@@ -50,7 +64,6 @@ async fn send_kafka() {
 #[rocket::main]
 async fn main() {
     let secret_key = SecretKey::generate(); // Genera una nueva clave secreta
-    let version = get_rdkafka_version();
 
     // Configuración de opciones CORS
     let cors = CorsOptions::default()
